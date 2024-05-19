ab9d4ef5de065aea5785d59380e4be861c65abd2
Thibaut <thibaut.stachnick@kairios.fr>
2024-05-19 10:10:22 +0200

    add a menu

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::io::{Read, stdin};
+use std::io::{stdin};
 use std::thread;
 use std::time::Duration;
 
@@ -26,11 +26,24 @@ fn init_todo()->TodoList{
     return TodoList::new()
 }
 
+fn show_menu(){
+    println!(
+        "\n
+        --------------------\n
+        | 1:Add a todo     |\n
+        | 2:Remove a todo  |\n
+        | 3:Modify a todo  |\n
+        | 4:Reset todolist |\n
+        --------------------"
+    )
+}
+
 fn run(){
     let todolist = init_todo();
     println!("What action to do with your todolist?");
     let mut user_input: String = String::new();
     stdin().read_line(& mut user_input).unwrap();
+    show_menu()
 }
 
 
