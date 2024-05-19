34bb199175b46e16c133cdfe3912552763ef7716
Thibaut <thibaut.stachnick@kairios.fr>
2024-05-19 10:03:30 +0200

    add a way to create a new to do list and to get user input

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,39 @@
+use std::io::{Read, stdin};
+use std::thread;
+use std::time::Duration;
+
 struct TodoList{
-    todos: [Todo]
+    todos: Vec<Todo>
 }
+
+impl TodoList {
+    fn new()->TodoList{
+        TodoList{
+            todos:Vec::new()
+        }
+    }
+}
+
 struct Todo{
     title:String,
     id:i64
 }
 
+fn init_todo()->TodoList{
+    println!("Wait...");
+    thread::sleep(Duration::from_secs(2));
+    println!("Todolist created!");
+    return TodoList::new()
+}
+
+fn run(){
+    let todolist = init_todo();
+    println!("What action to do with your todolist?");
+    let mut user_input: String = String::new();
+    stdin().read_line(& mut user_input).unwrap();
+}
 
 
 fn main() {
-    println!("Hello, world!");
+    run()
 }
