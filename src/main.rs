1fab6a58ba0534a55b1090c5e2d5604a854ba182
Thibaut Stachnick <thibautstachnick@gmail.com>
2024-06-10 18:58:44 +0200

    connect to database with new function

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::io::{stdin};
 use std::thread;
 use std::time::Duration;
+use postgres::{Client, NoTls, Error};
 
  struct TodoList{
     todos: Vec<Todo>
@@ -162,7 +163,16 @@ fn run(todolist: &mut TodoList, todo_index:usize){
 
 
 fn main() {
+
+    connect_to_database();
     let todo_index: usize = 0;
     let mut todolist = init_todo();
     run(&mut todolist,todo_index)
+}
+
+fn connect_to_database() {
+    let mut client
+        = Client::connect("postgresql://todorustuser:postgres@todorust/library", NoTls)?;
+    
+
 }
